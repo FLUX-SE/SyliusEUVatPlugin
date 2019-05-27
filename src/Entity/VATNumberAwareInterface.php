7aaf7bcb74df93d810914dde84dc6766fbc6d8fe
Francis Hilaire <Prometee@users.noreply.github.com>
2019-05-27 15:06:06 +0200

    Change from annotation to xml mapping for constraint

--- src/Entity/VATNumberAwareInterface.php
+++ src/Entity/VATNumberAwareInterface.php
@@ -4,13 +4,9 @@ declare(strict_types=1);
 
 namespace Prometee\SyliusVIESClientPlugin\Entity;
 
-use Prometee\VIESClientBundle\Constraints\VatNumber;
-
 interface VATNumberAwareInterface
 {
     /**
-     * @VatNumber(groups={"Default", "sylius"})
-     *
      * @return string|null
      */
     public function getVatNumber(): ?string;
