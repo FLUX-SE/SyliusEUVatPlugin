daa9ec4a5f219987798ecd33f75305a7e9acd92f
Francis Hilaire <Prometee@users.noreply.github.com>
2019-10-03 15:57:43 +0200

    Fix Iterator

--- src/Form/Extension/VATNumberTypeExtension.php
+++ src/Form/Extension/VATNumberTypeExtension.php
@@ -36,6 +36,6 @@ class VATNumberTypeExtension extends AbstractTypeExtension
      */
     public function getExtendedType()
     {
-        return self::getExtendedTypes()[0];
+        return self::getExtendedTypes()->current();
     }
 }
