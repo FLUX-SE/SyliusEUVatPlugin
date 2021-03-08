142ae25077991d18df2662e275fc9e5adc957ac5
Francis Hilaire <Prometee@users.noreply.github.com>
2021-03-08 18:19:52 +0100

    Fix copy/paste...

--- tests/Application/Kernel.php
+++ tests/Application/Kernel.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tests\FluxSE\SyliusPayumStripePlugin\Application;
+namespace Tests\FluxSE\SyliusEUVatPlugin\Application;
 
 use PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer;
 use Sylius\Bundle\CoreBundle\Application\Kernel as SyliusKernel;
