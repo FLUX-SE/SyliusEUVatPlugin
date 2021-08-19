d111d8e7e9541ac406d2dcace432ecd5417f2473
Francis Hilaire <Prometee@users.noreply.github.com>
2021-08-19 16:57:56 +0200

    Fix ECS wrongly configured fixer

--- ecs.php
+++ ecs.php
@@ -2,8 +2,16 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__.'/vendor/symplify/easy-coding-standard/config/set/symfony.php');
+
+    $services = $containerConfigurator->services();
+
+    /**
+     * Miss configured fixer into sylius-labs/coding-standard v4.1.0
+     */
+    $services->set(ClassAttributesSeparationFixer::class);
 };
