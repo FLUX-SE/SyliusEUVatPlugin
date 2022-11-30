40b26595f131cc203f5f72ff400b51a409e00d8e
Francis Hilaire <Prometee@users.noreply.github.com>
2022-11-30 21:16:28 +0100

    Use dist everywhere

--- ecs.php
+++ ecs.php
@@ -2,16 +2,8 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__.'/vendor/symplify/easy-coding-standard/config/set/symfony.php');
-
-    $services = $containerConfigurator->services();
-
-    /**
-     * Miss configured fixer into sylius-labs/coding-standard v4.1.0
-     */
-    $services->set(ClassAttributesSeparationFixer::class);
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->import(__DIR__ . '/vendor/sylius-labs/coding-standard/ecs.php');
 };
