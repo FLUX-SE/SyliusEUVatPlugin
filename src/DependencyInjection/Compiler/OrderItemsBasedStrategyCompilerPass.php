59143395b96e5abfaf6369de635e895ca70b2134
Francis Hilaire <Prometee@users.noreply.github.com>
2022-11-30 23:05:28 +0100

    Add retro compat for Sf 4.4

--- src/DependencyInjection/Compiler/OrderItemsBasedStrategyCompilerPass.php
+++ src/DependencyInjection/Compiler/OrderItemsBasedStrategyCompilerPass.php
@@ -13,8 +13,9 @@ final class OrderItemsBasedStrategyCompilerPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         $definition = $container->getDefinition('sylius.taxation.order_items_based_strategy');
-        $arg = $definition->getArgument(1);
-        $arg[] = new Reference('flux_se.sylius_eu_vat_plugin.applicator.order_european_vatnumber_applicator');
-        $definition->setArgument(1, $arg);
+        /** @var array $applicators */
+        $applicators = $definition->getArgument(1);
+        $applicators[] = new Reference('flux_se.sylius_eu_vat_plugin.applicator.order_european_vatnumber_applicator');
+        $definition->setArgument(1, $applicators);
     }
 }
