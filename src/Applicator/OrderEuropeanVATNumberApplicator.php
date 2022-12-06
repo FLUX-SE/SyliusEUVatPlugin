0178c7a234d545c9e548cad7ac774cfb3bf89d6c
Francis Hilaire <Prometee@users.noreply.github.com>
2022-12-06 18:19:33 +0100

    Fix ecs

--- src/Applicator/OrderEuropeanVATNumberApplicator.php
+++ src/Applicator/OrderEuropeanVATNumberApplicator.php
@@ -29,7 +29,7 @@ final class OrderEuropeanVATNumberApplicator implements OrderTaxesApplicatorInte
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      *
      * @throws InvalidArgumentException
      */
--- src/Constraints/CountryVatNumber.php
+++ src/Constraints/CountryVatNumber.php
@@ -13,7 +13,6 @@ class CountryVatNumber extends Constraint
 {
     public const CORRESPONDENCE_ERROR = '403';
 
-    /** @var string */
     public string $message = 'flux_se.sylius_eu_vat.country_vat_number.invalid';
 
     /** @var string[] */
