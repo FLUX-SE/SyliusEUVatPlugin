79409c439255779c43bd1e966ad6e1fab26dfadf
Francis Hilaire <Prometee@users.noreply.github.com>
2025-02-24 12:07:46 +0100

    Remove no more needed compiler passes

--- src/FluxSESyliusEUVatPlugin.php
+++ src/FluxSESyliusEUVatPlugin.php
@@ -4,23 +4,12 @@ declare(strict_types=1);
 
 namespace FluxSE\SyliusEUVatPlugin;
 
-use FluxSE\SyliusEUVatPlugin\DependencyInjection\Compiler\OrderItemsBasedStrategyCompilerPass;
-use FluxSE\SyliusEUVatPlugin\DependencyInjection\Compiler\OrderItemUnitsBasedStrategyCompilerPass;
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FluxSESyliusEUVatPlugin extends Bundle
 {
     use SyliusPluginTrait;
-
-    public function build(ContainerBuilder $container): void
-    {
-        $container->addCompilerPass(new OrderItemsBasedStrategyCompilerPass());
-        $container->addCompilerPass(new OrderItemUnitsBasedStrategyCompilerPass());
-
-        parent::build($container);
-    }
 
     public function getPath(): string
     {
