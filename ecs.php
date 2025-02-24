a3a24aa91d220fccd7dd91eec6a88b6b7bd210f2
Francis Hilaire <Prometee@users.noreply.github.com>
2025-02-24 12:07:14 +0100

    Update ecs config

--- ecs.php
+++ ecs.php
@@ -5,5 +5,12 @@ declare(strict_types=1);
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->import(__DIR__ . '/vendor/sylius-labs/coding-standard/ecs.php');
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests/Behat',
+        __DIR__ . '/tests/Unit',
+        __DIR__ . '/ecs.php',
+    ]);
+
+    $ecsConfig->import('vendor/sylius-labs/coding-standard/ecs.php');
 };
