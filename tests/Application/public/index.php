9661742176a3ce816ccc79c2785984c6fd1a89ba
Francis Hilaire <Prometee@users.noreply.github.com>
2025-02-24 18:56:01 +0100

    Sync config with Sylius-Standard 2.0

--- tests/Application/public/index.php
+++ tests/Application/public/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use Tests\FluxSE\SyliusEUVatPlugin\Application\Kernel;
+use Tests\FluxSE\SyliusEUVatPlugin\App\Kernel;
 
 require_once dirname(__DIR__) . '/../../vendor/autoload_runtime.php';
 
