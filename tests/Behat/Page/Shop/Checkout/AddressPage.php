f0ba8bf396146ff3d7772b79d69e60d0678e017c
Francis Hilaire <Prometee@users.noreply.github.com>
2026-07-28 23:47:22 +0200

    Enhance VAT number handling in checkout process by adding methods to specify and validate shipping and billing VAT numbers

--- tests/Behat/Page/Shop/Checkout/AddressPage.php
+++ tests/Behat/Page/Shop/Checkout/AddressPage.php
@@ -6,6 +6,7 @@ namespace Tests\FluxSE\SyliusEUVatPlugin\Behat\Page\Shop\Checkout;
 
 use Behat\Mink\Exception\ElementNotFoundException;
 use Sylius\Behat\Page\Shop\Checkout\AddressPage as BaseAddressPage;
+use Sylius\Behat\Service\DriverHelper;
 
 class AddressPage extends BaseAddressPage implements AddressPageInterface
 {
@@ -26,6 +27,14 @@ class AddressPage extends BaseAddressPage implements AddressPageInterface
     public function specifyShippingVatNumber(string $vatNumber): void
     {
         $this->waitForElementUpdate('form');
+        DriverHelper::waitForElement($this->getSession(), '[data-test-shipping-address] [data-test-vat-number]');
+        $this->getElement('shipping_vat_number')->setValue($vatNumber);
+    }
+
+    public function tryToSpecifyShippingVatNumber(string $vatNumber): void
+    {
+        $this->waitForElementUpdate('form');
+        DriverHelper::waitForElement($this->getSession(), '[data-test-shipping-address] [data-test-vat-number]', 1000);
 
         try {
             $this->getElement('shipping_vat_number')->setValue($vatNumber);
@@ -37,6 +46,14 @@ class AddressPage extends BaseAddressPage implements AddressPageInterface
     public function specifyBillingVatNumber(string $vatNumber): void
     {
         $this->waitForElementUpdate('form');
+        DriverHelper::waitForElement($this->getSession(), '[data-test-billing-address] [data-test-vat-number]');
+        $this->getElement('billing_vat_number')->setValue($vatNumber);
+    }
+
+    public function tryToSpecifyBillingVatNumber(string $vatNumber): void
+    {
+        $this->waitForElementUpdate('form');
+        DriverHelper::waitForElement($this->getSession(), '[data-test-billing-address] [data-test-vat-number]', 1000);
 
         try {
             $this->getElement('billing_vat_number')->setValue($vatNumber);
@@ -57,5 +74,12 @@ class AddressPage extends BaseAddressPage implements AddressPageInterface
         $this->waitForElementUpdate('form');
 
         return $this->hasElement('billing_vat_number');
+    }
+
+    public function nextStep(): void
+    {
+        $this->waitForElementUpdate('form');
+        $this->getElement('next_step')->press();
+        DriverHelper::waitForPageToLoad($this->getSession());
     }
 }
